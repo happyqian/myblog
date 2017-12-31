06e55669eb5c1157828f59c7ac519d9b408bb61a
happyqian_ah <happyqian123@gmail.com>
2017-12-31 13:00:21 +0800

    deploy5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    www.google.com
+                    www.baidu.com
                 </div>
 
                
