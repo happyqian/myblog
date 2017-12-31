80bcb0fe48f759f45448f2120d1db0b6607c99cf
happyqian_ah <happyqian123@gmail.com>
2017-12-31 12:06:38 +0800

    deploy3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    «Æ»Ÿ÷“
+                    www.fangqiaqia.com
                 </div>
 
                
